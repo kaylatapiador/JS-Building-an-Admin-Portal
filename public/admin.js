ffaa20a820ea9aa7315836de6585e723471ff0d2
kayla tapiador <kaylatapiador@yahoo.com>
2024-02-10 16:53:32 -0800

    activity updated

--- public/admin.js
+++ public/admin.js
@@ -1,2 +1,40 @@
 
 // Your Code Here
+async function main(){
+    let books = await fetch('http://localhost:3001/listBooks')
+    let booksJSON = await books.json()
+
+    booksJSON.forEach(function(book){
+        let div = document.createElement('div')
+        div.innerHTML = `
+        <img src = "${book.imageURL}" width="200" />
+        <h3 id="title-${book.id}">${book.title}</h3>
+        <p>Year pushlished: ${book.year}</p>
+        <p>Quantity: ${book.quantity}</p>
+        <input id="${book.id}" type="text" />
+        <input type="submit" onclick="changeTitle(${book.id})" />
+    `
+    document.body.append(div)
+    })
+}
+
+main()
+
+async function changeTitle(id){
+    let input = document.getElementById(id)
+    let value = input.value
+
+    let response = await fetch('https://localhost:3001/updateBook',
+        {
+            method:'PATCH',
+            headers:{
+                'Content-Type':'application/json'
+            },
+            book:JSON.stringify({
+                id:id,
+                title:value
+            })
+        })
+    let responseJson = await response.json()
+    document.getElementById(`title-${id}`).textContent = value
+}
